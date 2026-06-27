ac134790d7bc1289af87da90bae08aa268956ec6
enganisamohamed <enganisamohamed@gmail.com>
2026-06-27 14:02:52 -0700

    React Exercise 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import ShoppingCart from './ShoppingCart';
+import { useState } from "react";
 
-const App = () => {
+function App() {
+  const [name, setName] = useState("");
+  const [greeting, setGreeting] = useState("");
+
+  const [displayName, setDisplayName] = useState("");
+  const [displayGreeting, setDisplayGreeting] = useState("");
+
+  const showGreeting = () => {
+    setDisplayName(name);
+    setDisplayGreeting(greeting);
+  };
+
   return (
     <div>
-      <ShoppingCart />
+      <h1>Enter Your Name:</h1>
+
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <h1>Choose a Greeting:</h1>
+
+      <input
+        type="text"
+        value={greeting}
+        onChange={(e) => setGreeting(e.target.value)}
+      />
+
+
+
+      <h2>{displayGreeting} {displayName}</h2>
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/Greeting.jsx
@@ -0,0 +1,9 @@
+function Greeting({ greeting, name }) {
+  return (
+    <h2>
+      {greeting} {name}
+    </h2>
+  );
+}
+
+export default Greeting;
