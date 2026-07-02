56b4188e48fce4e30e21a9039a5446468de9dbd0
enganisamohamed <enganisamohamed@gmail.com>
2026-07-02 10:59:32 -0700

    React Exercise 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import { useState } from "react";
+import CountdownTimer from "./CountdownTimer";
 
 function App() {
-  const [mouse, setMouse] = useState({
-    x: 0,
-    y: 0,
-  });
-
-  const handleMouseMove = (e) => {
-    setMouse((prev) => ({
-      x: e.clientX !== prev.x ? e.clientX : prev.x,
-      y: e.clientY !== prev.y ? e.clientY : prev.y,
-    }));
-  };
-
   return (
-    <div
-      onMouseMove={handleMouseMove}
-      style={{ height: "100vh" }}
-    >
-      <h2>Mouse X: {mouse.x}</h2>
-      <h2>Mouse Y: {mouse.y}</h2>
+    <div>
+      <CountdownTimer />
     </div>
   );
 }
